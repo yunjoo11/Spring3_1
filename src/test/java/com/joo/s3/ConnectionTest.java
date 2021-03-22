d18d16f19bfa80f7331ba47597db4a45022beb28
yunjoo11 <qodbswn11@naver.com>
2021-03-22 16:29:40 +0900

    Initial commit

---
+++ src/test/java/com/joo/s3/ConnectionTest.java
@@ -0,0 +1,14 @@
+package com.joo.s3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConnectionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/joo/s3/MyAbstractTest.java
@@ -0,0 +1,14 @@
+package com.joo.s3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyAbstractTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
