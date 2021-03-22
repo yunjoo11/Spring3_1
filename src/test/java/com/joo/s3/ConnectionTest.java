fa2e6b4929909ead55b670678d782a9cc0451c5a
yunjoo11 <qodbswn11@naver.com>
2021-03-22 16:31:35 +0900

    1

--- src/test/java/com/joo/s3/ConnectionTest.java
+++ src/test/java/com/joo/s3/ConnectionTest.java
@@ -2,13 +2,19 @@ package com.joo.s3;
 
 import static org.junit.Assert.*;
 
+import javax.sql.DataSource;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class ConnectionTest {
-
+public class ConnectionTest extends MyAbstractTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void connectionTest() throws Exception{
+		assertNotNull(dataSource.getConnection());
 	}
-
+	
 }
--- src/test/java/com/joo/s3/MyAbstractTest.java
+++ src/test/java/com/joo/s3/MyAbstractTest.java
@@ -3,7 +3,10 @@ package com.joo.s3;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
 public class MyAbstractTest {
 
 	@Test
