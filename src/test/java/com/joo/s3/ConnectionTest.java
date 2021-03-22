470d95d379178692effda83f34e841dd6e26cb58
yunjoo11 <qodbswn11@naver.com>
2021-03-22 17:33:06 +0900

    1

--- src/test/java/com/joo/s3/ConnectionTest.java
+++ src/test/java/com/joo/s3/ConnectionTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,10 +12,12 @@ public class ConnectionTest extends MyAbstractTest {
 	
 	@Autowired
 	private DataSource dataSource;
+	@Autowired
+	private SqlSession sqlSession;
 	
 	@Test
 	public void connectionTest() throws Exception{
-		assertNotNull(dataSource.getConnection());
+		assertNotNull(sqlSession.getConnection());
 	}
 	
 }
