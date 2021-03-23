da56abc18daf3e42b23dfe2c5033993935178caf
yunjoo11 <qodbswn11@naver.com>
2021-03-23 16:23:22 +0900

    1

--- src/main/java/com/joo/s3/member/MemberDAO.java
+++ src/main/java/com/joo/s3/member/MemberDAO.java
@@ -17,23 +17,23 @@ public class MemberDAO {
 	private final String NAMESPACE="com.joo.s3.member.MemberDAO";
 	
 	public int memberUpdate(MemberDTO memberDTO)throws Exception{
-		return sqlSession.update(NAMESPACE+"memberUpdate", memberDTO);		
+		return sqlSession.update(NAMESPACE+".memberUpdate", memberDTO);		
 	}	
 	
 	public int memberDelete(MemberDTO memberDTO)throws Exception{
-		return sqlSession.delete(NAMESPACE+"memberDelete", memberDTO);
+		return sqlSession.delete(NAMESPACE+".memberDelete", memberDTO);
 		
 	}
 
 	//memberJoin 데이터를 받아서 db에 insert하는 메서드
 	public int memberJoin(MemberDTO memberDTO)throws Exception{
-		int result = sqlSession.insert(NAMESPACE+"memberJoin",memberDTO);
+		int result = sqlSession.insert(NAMESPACE+".memberJoin",memberDTO);
 		return result;		
 	}
 	
 	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception{
 		
-		memberDTO=sqlSession.selectOne(NAMESPACE+"memberLogin", memberDTO);
+		memberDTO=sqlSession.selectOne(NAMESPACE+".memberLogin", memberDTO);
 		
 		return memberDTO;
 	}
